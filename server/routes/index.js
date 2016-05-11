1aa89ccf94fadc3901ceeeec188e71e4b9fb96e4
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-05-11 12:40:49 -0600

    added the posts route that returns all posts with a nice array of comments. Added layout folder on client side

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,4 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'New Reddit Clone' });
 });
 
+router.get('/posts', function(req, res, next){
+  knex('posts').then(function(posts){
+    knex('comments').then(function(comments){
+      for (var i = 0; i < posts.length; i++) {
+        posts[i].comments = [];
+        for (var j = 0; j < comments.length; j++) {
+          if(comments[j].post_id === posts[i].id){
+            posts[i].comments.push(comments[j].comment);
+          }
+        }
+      }
+      res.json(posts);
+    });
+  });
+});
 module.exports = router;
